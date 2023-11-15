37971185e11f0932997db3a8e54a9a08eab7e6b7
martyall <martyall@protonmail.com>
2023-11-14 22:29:56 -0800

    parsing and interpreting statements

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ pub mod interpreter;
 pub mod parser;
 
 use clap::Parser;
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::Read;
 
@@ -21,13 +22,17 @@ fn main() {
     let mut file = File::open(args.input_file).unwrap();
     let mut contents = String::new();
     file.read_to_string(&mut contents).unwrap();
-    let expr = parser::parse(&contents).unwrap();
+    let statements = parser::parse(&contents).unwrap();
 
     if args.serialize {
-        let serialized = serde_json::to_string(&expr).unwrap();
+        let serialized = serde_json::to_string(&statements).unwrap();
         println!("{}", serialized);
     } else {
-        let result = interpreter::interpret(&expr);
-        println!("Your result is: {}", result);
+        let mut context = HashMap::new();
+        statements.iter().for_each(|statement| {
+            interpreter::interpret(&mut context, &statement);
+        });
+
+        println!("Your result is: {:?}", context.get("main").unwrap());
     }
 }

050141015f657b5467a699c8b525b9d4077896e8
martyall <martyall@protonmail.com>
2023-11-27 23:26:43 -0800

    rexport inliner

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -1,7 +1,9 @@
 pub mod expression;
+pub mod inliner;
 pub mod interpreter;
 pub mod prim;
 
 pub use expression::{ast_expr_to_lambda_expr, LambdaExpr};
+pub use inliner::inline;
 pub use interpreter::evaluate;
 pub use prim::prim;
